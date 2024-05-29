3635a7edec6df7d3ba5cb0daf4fa83d3f2d566dc
Deras879 <derycard59@gmail.com>
2024-05-28 20:30:32 -0500

    Implementado zustand, y empezado footer

---
+++ src/features/Main/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div>
+        <a href="">Quienes somos?</a>
+        <a href="">Privacidad</a>
+        <a href="">Top comidas</a>
+        <a href="">Top ciudades</a>
+        <a href="">Registra tu negocio</a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
