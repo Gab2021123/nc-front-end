1caa82e60ac20376981781756ef7de9fb9a85aa0
Deras879 <derycard59@gmail.com>
2024-06-04 23:17:52 -0500

    Arreglado estilos del footer

--- src/features/Main/components/Footer/Footer.tsx
+++ src/features/Main/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-orange-500  col-start-1 col-end-13">
-      <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <div className="flex space-x-4">
+      <div className="container mx-auto px-6 py-3 flex flex-col justify-between items-center font-semibold">
+        <nav className="flex space-x-4 justify-between w-full p-[10px] ">
           <Link className="flex justify-center items-center text-white " to="">
             Quienes somos
           </Link>
@@ -23,16 +23,16 @@ const Footer = () => {
           <Link className="flex justify-center items-center text-white " to="">
             Se parte de FlashFood
           </Link>
-        </div>
-        <hr className="my-3 border-t border-white w-6 mx-auto" />
-        <div className="text-center">
-          <div className="mb-2">
+        </nav>
+        <hr className="my-3 border-t border-white w-full mx-auto" />
+        <div className="text-center flex justify-evenly items-center w-full">
+          <div className="flex flex-row gap-[25px] items-center justify-center">
             <h3 className="text-white">Te arrepentiste de la compra?</h3>
             <button className="bg-white text-orange-500 px-4 py-2 rounded">
               Boton de arrepentimiento
             </button>
           </div>
-          <div className="text-white">
+          <div className="text-white text-left flex flex-col gap-2">
             <p>
               Delivery hero E-commerce S.A. Cuit 30-42930149-6 | Av. Libertador
               7330, piso 14, ciudad
